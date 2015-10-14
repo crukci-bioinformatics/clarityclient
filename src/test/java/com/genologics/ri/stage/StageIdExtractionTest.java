94b0f8e53b88695aa568f999f3e14b599f73dd68
Richard Bowers <richard.bowers@cruk.cam.ac.uk>
2015-10-14 14:22:27 +0100

    Add copyright messages.

--- src/test/java/com/genologics/ri/stage/StageIdExtractionTest.java
+++ src/test/java/com/genologics/ri/stage/StageIdExtractionTest.java
@@ -1,3 +1,21 @@
+/*
+ * CRUK-CI Genologics REST API Java Client.
+ * Copyright (C) 2013 Cancer Research UK Cambridge Institute.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.genologics.ri.stage;
 
 import static org.junit.Assert.*;
