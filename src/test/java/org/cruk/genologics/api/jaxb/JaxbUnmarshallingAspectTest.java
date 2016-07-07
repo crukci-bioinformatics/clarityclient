a69c6e88e3df4b71a80af031f567a17520c65a5c
Richard Bowers <richard.bowers@cruk.cam.ac.uk>
2016-07-07 11:35:44 +0100

    Renamed GenologicsExceptionAspectTest to JaxbUnmarshallingAspectTest.

--- src/test/java/org/cruk/genologics/api/jaxb/JaxbUnmarshallingAspectTest.java
+++ src/test/java/org/cruk/genologics/api/jaxb/JaxbUnmarshallingAspectTest.java
@@ -11,7 +11,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -31,11 +31,11 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
-public class GenologicsExceptionAspectTest
+public class JaxbUnmarshallingAspectTest
 {
     protected Jaxb2Marshaller marshaller;
 
-    public GenologicsExceptionAspectTest()
+    public JaxbUnmarshallingAspectTest()
     {
         ApplicationContext context = UnitTestApplicationContextFactory.getApplicationContext();
         marshaller = context.getBean("genologicsJaxbMarshaller", Jaxb2Marshaller.class);
