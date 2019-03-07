0e33afb3a4b8566f5ddff8f5ed5a8eeb6b836b2c
Richard Bowers <richard.bowers@cruk.cam.ac.uk>
2019-03-07 15:03:12 +0000

    Constructor on dimension for convenience.

--- src/main/java/com/genologics/ri/containertype/Dimension.java
+++ src/main/java/com/genologics/ri/containertype/Dimension.java
@@ -11,7 +11,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -53,6 +53,15 @@ public class Dimension implements Serializable
     protected Integer offset;
 
     protected Integer size;
+
+    public Dimension()
+    {
+    }
+
+    public Dimension(Integer size)
+    {
+        this.size = size;
+    }
 
     public Boolean isAlpha()
     {
